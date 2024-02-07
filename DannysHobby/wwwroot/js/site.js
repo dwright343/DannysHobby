c0c1c1e7c493c73833108f3775a475e2b6a30fef
dwright6633 <103792319+dwright6633@users.noreply.github.com>
2024-02-07 14:26:42 -0700

    Add project files.

---
+++ DannysHobby/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$("#calcbtn").click(function ()
+    {
+    var hours = $("#hoursInput").val();
+    var hourlyRate = parseFloat($("#hourlyRate").val().replace("$", ""));
+
+    var total = hours * hourlyRate;
+    $("#totalOutput").html("$" + total.toFixed(2));
+    }
+)
